1a3733a9e97b8ec8b4251a5f3767d1e050b8a506
José Neto <devneto203@gmail.com>
2023-07-06 22:58:57 -0300

    Add cpf validation

--- funcoes.c
+++ funcoes.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "validarCPF.C"
+
 
 typedef struct{
     char nome[100];
@@ -56,6 +56,48 @@ void nacionalidades() {
     printf("Opcao: ");
 }
 
+// Validando o CPF
+int validarCPF(char *cpf){
+    int i, j, digito1 = 0, digito2 = 0;
+    if(strlen(cpf) != 11){
+        return 0;
+    } else if((strcmp(cpf,"00000000000") == 0) || (strcmp(cpf,"11111111111") == 0) || (strcmp(cpf,"22222222222") == 0) ||
+            (strcmp(cpf,"33333333333") == 0) || (strcmp(cpf,"44444444444") == 0) || (strcmp(cpf,"55555555555") == 0) ||
+            (strcmp(cpf,"66666666666") == 0) || (strcmp(cpf,"77777777777") == 0) || (strcmp(cpf,"88888888888") == 0) ||
+            (strcmp(cpf,"99999999999") == 0)){
+                return 0; ///se o CPF tiver todos os números iguais ele é inválido.
+    } else {
+        ///digito 1---------------------------------------------------
+        for(i = 0, j = 10; i < strlen(cpf)-2; i++, j--){///multiplica os números de 10 a 2 e soma os resultados dentro de digito1
+            digito1 += (cpf[i]-48) * j;
+        }
+        digito1 %= 11;
+        if(digito1 < 2){
+            digito1 = 0;
+        } else {
+            digito1 = 11 - digito1;
+        }
+            
+        if((cpf[9]-48) != digito1)
+            return 0; ///se o digito 1 não for o mesmo que o da validação CPF é inválido
+        else
+        ///digito 2--------------------------------------------------
+        {
+            for(i = 0, j = 11; i < strlen(cpf)-1; i++, j--) ///multiplica os números de 11 a 2 e soma os resultados dentro de digito2
+                    digito2 += (cpf[i]-48) * j;
+        digito2 %= 11;
+        if(digito2 < 2)
+            digito2 = 0;
+        else
+            digito2 = 11 - digito2;
+        if((cpf[10]-48) != digito2)
+            return 0; ///se o digito 2 não for o mesmo que o da validação CPF é inválido
+        }
+    }
+    return 1;
+}
+
+// Calculando IMC
 float calcIMC(float p, float a){
     return p/(a*a);
 }
@@ -103,7 +145,7 @@ int cadastrar_pessoa(Pessoa **p, int tam, int qtd){
             getchar();
 
             if(!validarCPF(novaPessoa->cpf)){
-                printf("\n\tCPF Invalido! Tente Novamente.");
+                printf("\n\tCPF Invalido! Tente Novamente.\n");
             }
         } while(!validarCPF(novaPessoa->cpf));
 
